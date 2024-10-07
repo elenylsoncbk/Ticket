90642166986c95cd430dcd1bfe1b4e0ee43743d2
elenylsoncbk <elenylsoncm@gmail.com>
2024-10-07 11:19:15 -0300

    Update Trabalho1Sm.java

--- Trabalho1Sm.java
+++ Trabalho1Sm.java
@@ -1,9 +1,8 @@
-package oo.orientacao.DesafioDaSemana;
-
+package oo.Composicao.Desafio.teste;
 import java.util.ArrayList;
 import java.util.Scanner;
+public class Trabalho1 {
 
-public class Trabalho1Sm {
     public static void main(String[] args) {
         final double valor = 4.0;
         boolean status = false;
@@ -15,56 +14,57 @@ public class Trabalho1Sm {
             System.out.println();
             System.out.println("      -MENU-      ");
             System.out.print("""
-                    1 - Para Adicionar:
-                    2 - Para Consultar Saldo:
-                    3 - Para Pagar:
-                    4 - Para Sair:
-                    """);
+                   1 - Para Adicionar:
+                   2 - Para Consultar Saldo:
+                   3 - Para Pagar:
+                   4 - Para Sair:
+                   """);
             comand = entrada.nextLine().trim();
             double valorFinal = 0;
-                switch (comand) {
-                    case "1" -> {
-                        cValor.add(valor);
-                        cStatus.add(status);
-                        System.out.println("Ticket Gerado! ");
-                    }
-                    case "2" -> {
-                        for (boolean c : cStatus) {
-                            if (c) {
-                                valorFinal += 4;
-                            }
-                        }
-                        System.out.println(valorFinal);
-                    }
-                    case "3" -> {
-                        int ty = 0;
-                        for (boolean d : cStatus) {
-                            if (d) {
-                                System.out.println(ty + " Foi Pago => " + cValor.get(ty));
-                            } else {
-                                System.out.println(ty + " Não Foi Pago ");
-                            }
-                            ty++;
-                        }
-                        try {
-                            System.out.println();
-                            System.out.println("informe qual deseja concluir: ");
-                            int subConcluidas = entrada.nextInt();
-                            if (subConcluidas > cStatus.size() - 1) {
-                                System.out.println("Indice Invalido!");
-                            } else {
-                                cStatus.set(subConcluidas, true);
-                            }
-                            System.out.println("----------------\n");
-                            entrada.nextLine();
-                        } catch (Exception e) {
-                            System.out.println("Indice Invalido!");
-                            System.out.println("----------------\n");
-                        }
-                    }
-                    case "4" -> System.out.println("FINAL");
-                    default -> System.out.println("invalido");
+            switch (comand) {
+                case "1" -> {
+                    cValor.add(valor);
+                    cStatus.add(status);
+                    System.out.println("Ticket Gerado! ");
                 }
+                case "2" -> {
+                    for (boolean c : cStatus) {
+                        if (c) {
+                            valorFinal += 4;
+                        }
+                        }
+                    System.out.println(valorFinal);
+                    }
+
+                case "3" -> {
+                    int ty = 0;
+                    for (boolean d : cStatus) {
+                        if (d) {
+                            System.out.println(ty + " - Foi Pago => " + cValor.get(ty));
+                        } else {
+                            System.out.println(ty + " - Não Foi Pago ");
+                        }
+                        ty++;
+                        }
+                    try {
+                        System.out.println();
+                        System.out.println("informe qual deseja concluir: ");
+                        int subConcluidas = entrada.nextInt();
+                        if (subConcluidas > cStatus.size() - 1) {
+                            System.out.println("Indice Invalido!");
+                        } else {
+                            cStatus.set(subConcluidas, true);
+                        }
+                        System.out.println("----------------\n");
+                        entrada.nextLine();
+                    } catch (Exception e) {
+                        System.out.println("Indice Invalido!");
+                        System.out.println("----------------\n");
+                    }
+                }
+                case "4" -> System.out.println("FINAL");
+                default -> System.out.println("invalido");
+            }
         }while (!comand.equalsIgnoreCase("4"));
         entrada.close();
     }
